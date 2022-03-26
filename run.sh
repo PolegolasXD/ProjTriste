c8ee4b8385111e89019e700b13c0ae9f9f864d53
PolegolasXD <polegolasplays@gmail.com>
2022-03-26 15:02:52 -0300

    feat: adding running script

---
+++ run.sh
@@ -0,0 +1,11 @@
+clear
+echo "Aviso, é recomendado usar o java na versão 11.0.14"
+echo "A sua versão do java é: "
+echo
+java --version
+
+javac Main.java
+java Main
+
+rm Main.class
+rm models/Usuarios.class
